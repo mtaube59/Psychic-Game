9c101cfa4fbebad642212e090308ca82e96cbd34
Matthew Taube <taubemj@gmail.com>
2019-03-12 20:11:11 -0400

    basics

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var wins = 0;
+var losses = 0;
+var lettersTried ;
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("left-text");
+var userGuessesText = document.getElementById("guesses-text");
